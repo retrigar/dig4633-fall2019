966fca8107098a7be76fa43f845c850cd946c230
retrigar <42656578+retrigar@users.noreply.github.com>
2019-09-25 21:10:52 -0400

    lab1

---
+++ lab-vr/lab1/Assets/GMngr.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GMngr : MonoBehaviour
+{
+    public bool AllApples = false;
+    public bool CowFull = false;
+    public int ACollect = 0;
+    public int CFed = 0;
+    public GameObject winText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.FindGameObjectsWithTag("apple");
+    }
+
+    public void ACount ()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AllApples && CowFull)
+        {
+            winText.SetActive(true);
+        }
+    }
+}
